2f6f405db6aa7553e3374dbabd58d4ef0a6e6d40
2685984789 <60116182+2685984789@users.noreply.github.com>
2020-01-20 22:35:58 +0100

    Update bdd.conf.php

--- config/bdd.conf.php
+++ config/bdd.conf.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8','AzStoinou','AzStoinou59');
+    $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8','root','root');
     $bdd->exec("set names utf8");
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
